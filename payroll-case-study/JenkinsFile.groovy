682002dc290c3e955dc94352bb0f2ebe672cd1db
José Reato Neto <josereato@outlook.com>
2018-11-18 19:24:02 -0200

    teste automatizados

--- payroll-case-study/JenkinsFile.groovy
+++ payroll-case-study/JenkinsFile.groovy
@@ -8,22 +8,21 @@ pipeline {
             
              steps {
                 
-                echo '### checkout ###'
+                echo '########## checkout ##########'
                 checkout scm
 
-                echo '### Restore Nuget Package ###'
+                echo '########## Restore Nuget Package ##########'
                 bat "C:\\Jenkins\\workspace\\nuget.exe restore C:\\Jenkins\\workspace\\Payroll3\\payroll-case-study\\Payroll.sln"
 
-                echo '### Building... ###' 
-                bat "msbuild.exe C:\\Jenkins\\workspace\\Payroll3\\payroll-case-study\\Payroll.sln"
-
-                
+                echo '########## Building... ##########' 
+                bat "msbuild.exe C:\\Jenkins\\workspace\\Payroll3\\payroll-case-study\\Payroll.sln" 
              }
 
         }
         stage('Test') {
             steps {
-                echo 'Testing..'
+                echo '########## Testing... ##########'
+                bat "C:\\Program Files (x86)\\NUnit.org\\nunit-console\\nunit3-console.exe C:\\Jenkins\\workspace\\Payroll3\\payroll-case-study\\Test\\bin\\Debug\\Test.dll /result=nunit-result.xml"
             }
         }
         stage('Deploy') {
@@ -31,5 +30,17 @@ pipeline {
                 echo 'Deploying....'
             }
         }
+    }
+
+    post {
+        success {
+            echo 'Pipeline Succeeded'
+        }
+        failure {
+            echo 'Pipeline Failed'
+        }
+        unstable {
+            echo 'Pipeline run marked unstable'
+        }       
     }
 }
