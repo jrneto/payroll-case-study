6d63526f39d5bed81eaa3ad69a7f0c01ac1beca5
José Reato Neto <josereato@outlook.com>
2018-11-19 14:49:14 -0200

    sonar

--- payroll-case-study/JenkinsFile.groovy
+++ payroll-case-study/JenkinsFile.groovy
@@ -19,6 +19,17 @@ pipeline {
              }
 
         }
+        stage('Build + SonarQube analysis') {
+            step {
+                def sqScannerMsBuildHome = tool 'SonarScanner-MsBuild'
+                withSonarQubeEnv('http://localhost:9000') {
+                    // Due to SONARMSBRU-307 value of sonar.host.url and credentials should be passed on command line
+                    bat "${sqScannerMsBuildHome}\\SonarQube.Scanner.MSBuild.exe begin /k:Payroll /n:Payroll /v:1.0 /d:sonar.host.url=http://localhost:9000 /d:sonar.login=bdb09369df36b21df17469fd14fc1490d88f7807"
+                    bat 'MSBuild.exe /t:Rebuild'
+                    bat "${sqScannerMsBuildHome}\\SonarQube.Scanner.MSBuild.exe end"
+                }
+            }
+        }
         stage('Test') {
             steps {
                 echo '########## Testing... ##########'
