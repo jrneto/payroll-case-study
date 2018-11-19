1d3acc1952f1161a842c172cd1be5001a1108a5f
José Reato Neto <josereato@outlook.com>
2018-11-19 17:59:06 -0200

    teste

--- payroll-case-study/JenkinsFile.groovy
+++ payroll-case-study/JenkinsFile.groovy
@@ -22,13 +22,19 @@ pipeline {
         }
         stage('Build + SonarQube analysis') {
             //The tool name "SonarQube Scanner 2.8" needs to match the "Name" field of a SonarQube Installation on the Global Tools Configuration page
-            def sqScannerMsBuildHome = tool 'Sonarqube-scanner3.2.0'
-            //The name used in the withSonarQubeEnv step needs to match the "Name" field of a SonarQube server defined on the Configure System page.
-            withSonarQubeEnv('sonar-msbuild') {
-                // Due to SONARMSBRU-307 value of sonar.host.url and credentials should be passed on command line
-                bat "${sqScannerMsBuildHome}\\SonarScanner.MSBuild.exe begin /k:payroll /d:sonar.host.url=%SONAR_HOST_URL% /d:sonar.login=%SONAR_AUTH_TOKEN%"
-                bat 'MSBuild.exe /t:Rebuild'
-                bat "${sqScannerMsBuildHome}\\SonarScanner.MSBuild.exe end"
+            steps {
+                script { 
+                    def sqScannerMsBuildHome = tool 'Sonarqube-scanner3.2.0'
+                    echo "===================================================================="
+                    //The name used in the withSonarQubeEnv step needs to match the "Name" field of a SonarQube server defined on the Configure System page.
+                    withSonarQubeEnv('sonar-msbuild') {
+                        echo "${sqScannerMsBuildHome}\\SonarScanner.MSBuild.exe begin /k:payroll /d:sonar.host.url=%SONAR_HOST_URL% /d:sonar.login=%SONAR_AUTH_TOKEN%"
+                        // Due to SONARMSBRU-307 value of sonar.host.url and credentials should be passed on command line
+                        bat "${sqScannerMsBuildHome}\\SonarScanner.MSBuild.exe begin /k:payroll /d:sonar.host.url=%SONAR_HOST_URL% /d:sonar.login=%SONAR_AUTH_TOKEN%"
+                        bat 'MSBuild.exe /t:Rebuild'
+                        bat "${sqScannerMsBuildHome}\\SonarScanner.MSBuild.exe end"
+                    }
+                }
             }
         }
 
